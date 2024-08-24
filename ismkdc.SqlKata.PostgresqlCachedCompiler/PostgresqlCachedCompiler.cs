9e7f4b39a14410af1e152dd63ab89ef8a1c15c95
İsmail Kundakcı <ism.kundakci@hotmail.com>
2024-08-25 01:00:10 +0300

    init

---
+++ ismkdc.SqlKata.PostgresqlCachedCompiler/PostgresqlCachedCompiler.cs
@@ -0,0 +1,35 @@
+﻿using SqlKata;
+using SqlKata.Compilers;
+
+namespace ismkdc.SqlKata.PostgresqlCachedCompiler;
+
+public class PostgresqlCachedCompiler : PostgresCompiler
+{
+    private static readonly Dictionary<ulong, SqlResult> Cache = new();
+
+    protected override SqlResult CompileRaw(Query query)
+    {
+        var hash = CalculateHash(Environment.StackTrace);
+
+        if (Cache.TryGetValue(hash, out var cached)) return cached;
+
+        var compiled = base.CompileRaw(query);
+
+        Cache[hash] = compiled;
+
+        return compiled;
+    }
+
+    private static ulong CalculateHash(string data)
+    {
+        var hashedValue = 3074457345618258791ul;
+
+        foreach (var t in data)
+        {
+            hashedValue += t;
+            hashedValue *= 3074457345618258799ul;
+        }
+
+        return hashedValue;
+    }
+}
